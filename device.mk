7112d097a0b8427f48776a204e8f7a523fe1a80a
aksbrar <69970288+aksbrar@users.noreply.github.com>
2024-01-23 19:58:28 +0530

    Update device.mk

--- device.mk
+++ device.mk
@@ -75,3 +75,5 @@ ENABLE_VIRTUAL_AB := true
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch.mk)
 
 TW_THEME := portrait_hdpi
+TARGET_SCREEN_HEIGHT := 2400
+TARGET_SCREEN_WIDTH := 1080
